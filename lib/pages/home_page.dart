2912b0a8dd3d133f6fc5e5c7491664237ec7fad9
xX_Nikolay_Xx <homecomp2018@gmail.com>
2023-08-05 13:09:09 +0500

    authentication persists until user logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,63 +1,21 @@
-import 'package:firebase_login_app/blocs/authentication/authentication_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_login_app/pages/login/login_page.dart';
 import 'package:firebase_login_app/pages/welcome_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late final TabController tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(
-      vsync: this,
-      length: 2,
-    );
-  }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
-
-  void loginTransition() {
-    if (tabController.index != 1) tabController.animateTo(1);
-  }
-
-  void logoutTransition() {
-    if (tabController.index != 0) tabController.animateTo(0);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
-      builder: (context, state) {
-        if (state is AuthenticationSuccess) {
-          loginTransition();
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data != null) {
+          return const WelcomePage();
+        } else {
+          return const LoginPage();
         }
-
-        if (state is AuthenticationRevoked) {
-          logoutTransition();
-        }
-
-        return TabBarView(
-          physics: const NeverScrollableScrollPhysics(),
-          controller: tabController,
-          children: const [
-            LoginPage(),
-            WelcomePage(),
-          ],
-        );
       },
     );
   }
