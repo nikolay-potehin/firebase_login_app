0c1d76c25386fe8c5afe7330ed7894032780e2f7
xX_Nikolay_Xx <homecomp2018@gmail.com>
2023-08-30 20:18:35 +0500

    minor refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_login_app/theme.dart';
 import 'package:firebase_login_app/firebase_options.dart';
 import 'package:firebase_login_app/pages/start_page.dart';
-import 'package:firebase_login_app/repositories/user_repository.dart';
 import 'package:firebase_login_app/models/utils.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,12 +11,7 @@ Future main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(
-    Provider(
-      create: (_) => UserRepository(),
-      child: const LoginApp(),
-    ),
-  );
+  runApp(const LoginApp());
 }
 
 class LoginApp extends StatelessWidget {
